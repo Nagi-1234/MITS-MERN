8ba269c84528272e31eec907fba236954f2f6c0a
Nagi <nageswari6498@gmail.com>
2026-01-23 10:18:42 +0530

    portfolio

--- script.js
+++ script.js
@@ -1,60 +1,58 @@
-// // // // // // // // // // // .log("Hello world!");
-// // // // // // // // // // // var a = 5;
-// // // // // // // // // // // a = 10;
-// // // // // // // // // // // console.log(a);
+// // // // // // // // // // // // .log("Hello world!");
+// // // // // // // // // // // // var a = 5;
+// // // // // // // // // // // // a = 10;
+// // // // // // // // // // // // console.log(a);
 
-// // // // // // // // // // //  let b = 20;
-// // // // // // // // // // // console.log(b);
+// // // // // // // // // // // //  let b = 20;
+// // // // // // // // // // // // console.log(b);
 
-// // // // // // // // // // // const c =30;
-// // // // // // // // // // // console.log(c)
-// // // // // // // // // // // console
-// // // // // // // // // // // var str = "nagi"
-// // // // // // // // // // // console.log(str)
+// // // // // // // // // // // // const c =30;
+// // // // // // // // // // // // console.log(c)
+// // // // // // // // // // // // console
+// // // // // // // // // // // // var str = "nagi"
+// // // // // // // // // // // // console.log(str)
 
-// // // // // // // // // // // var bool = true;
-// // // // // // // // // // // console.log(bool)
+// // // // // // // // // // // // var bool = true;
+// // // // // // // // // // // // console.log(bool)
 
-// // // // // // // // // // // var un; 
-// // // // // // // // // // // console.log(un)
+// // // // // // // // // // // // var un; 
+// // // // // // // // // // // // console.log(un)
 
-// // // // // // // // // // // var nu = null;
-// // // // // // // // // // // //console.log(nu);
+// // // // // // // // // // // // var nu = null;
+// // // // // // // // // // // // //console.log(nu);
 
-// // // // // // // // // // // //var big = 1235565645645n
-// // // // // // // // // // // //console.log(big)
+// // // // // // // // // // // // //var big = 1235565645645n
+// // // // // // // // // // // // //console.log(big)
 
-// // // // // // // // // // // //var symbol = Symbol("li")
-// // // // // // // // // // // //console.log(typeof symbol)
-
-
-// // // // // // // // // // var a = 10;
-// // // // // // // // // // var b = 20;
-// // // // // // // // // // console.log(a+b)
-// // // // // // // // // // console.log(a-b);
-// // // // // // // // // // console.log(a%b);
-
-// // // // // // // // // // //relational operators
-// // // // // // // // // // var a =10;
-// // // // // // // // // // var b =20;
-// // // // // // // // // // console.log(a<b)
-// // // // // // // // // // console.log(a>b);
-// // // // // // // // // // console.log(a>=b);
-// // // // // // // // // // console.log(a<=b);;
-// // // // // // // // // // console.log(a==b);
-// // // // // // // // // // console.log(a!=b);
-// // // // // // // // // // console.log(a===b);
-// // // // // // // // // // console.log(a!==b);
+// // // // // // // // // // // // //var symbol = Symbol("li")
+// // // // // // // // // // // // //console.log(typeof symbol)
 
 
-// // // // // // // // // // a =10;
-// // // // // // // // // // b="10";
-// // // // // // // // // // console.log(a==b);
-// // // // // // // // // // console.log(a!=b);
-// // // // // // // // // // console.log(a===b);
-// // // // // // // // // // console.log(a!==b);
+// // // // // // // // // // // var a = 10;
+// // // // // // // // // // // var b = 20;
+// // // // // // // // // // // console.log(a+b)
+// // // // // // // // // // // console.log(a-b);
+// // // // // // // // // // // console.log(a%b);
+
+// // // // // // // // // // // //relational operators
+// // // // // // // // // // // var a =10;
+// // // // // // // // // // // var b =20;
+// // // // // // // // // // // console.log(a<b)
+// // // // // // // // // // // console.log(a>b);
+// // // // // // // // // // // console.log(a>=b);
+// // // // // // // // // // // console.log(a<=b);;
+// // // // // // // // // // // console.log(a==b);
+// // // // // // // // // // // console.log(a!=b);
+// // // // // // // // // // // console.log(a===b);
+// // // // // // // // // // // console.log(a!==b);
 
 
+// // // // // // // // // // // a =10;
+// // // // // // // // // // // b="10";
+// // // // // // // // // // // console.log(a==b);
+// // // // // // // // // // // console.log(a!=b);
+// // // // // // // // // // // console.log(a===b);
+// // // // // // // // // // // console.log(a!==b);
 
 
 
@@ -64,133 +62,135 @@
 
 
 
-// // // // // // // // // a = true;
-// // // // // // // // // b= false;
-// // // // // // // // // console.log(a&&b);
-// // // // // // // // // console.log(a||b);
-// // // // // // // // // console.log(!a);
 
 
-// // // // // // // // // var arr = [10,20,30,40];
-// // // // // // // // // console.log(arr);
-// // // // // // // // // console.log(typeof arr);
+// // // // // // // // // // a = true;
+// // // // // // // // // // b= false;
+// // // // // // // // // // console.log(a&&b);
+// // // // // // // // // // console.log(a||b);
+// // // // // // // // // // console.log(!a);
 
-// // // // // // // // // var obj ={
-// // // // // // // // //   name: "nagi",
-// // // // // // // // //   age: 19
+
+// // // // // // // // // // var arr = [10,20,30,40];
+// // // // // // // // // // console.log(arr);
+// // // // // // // // // // console.log(typeof arr);
+
+// // // // // // // // // // var obj ={
+// // // // // // // // // //   name: "nagi",
+// // // // // // // // // //   age: 19
+// // // // // // // // // // }
+// // // // // // // // // // console.log(obj);
+// // // // // // // // // // console.log(typeof obj);
+
+// // // // // // // // // for(let i=0; i<=10; i++){
+// // // // // // // // //   console.log(i);
+  
 // // // // // // // // // }
-// // // // // // // // // console.log(obj);
-// // // // // // // // // console.log(typeof obj);
 
-// // // // // // // // for(let i=0; i<=10; i++){
+// // // // // // // // let i =1;
+// // // // // // // // while(i<=10){
 // // // // // // // //   console.log(i);
+// // // // // // // //   i++;
   
 // // // // // // // // }
 
-// // // // // // // let i =1;
-// // // // // // // while(i<=10){
-// // // // // // //   console.log(i);
-// // // // // // //   i++;
+
+
+// // // // // // // i = 1;
+// // // // // // // do{
+// // // // // // //   console.log("do..wile");
+  
+// // // // // // // }while(i===0);
+
+// // // // // // // for(let i =1; i<=10; i++){
+// // // // // // //   if(i%2==0)console.log(i);
   
 // // // // // // // }
+// // // // // // // i =9;
+// // // // // // // if(i%2==0) console.log("even");
+// // // // // // // else console.log("odd");
 
+// // // // // // let mark = 70;
+// // // // // // if(mark>=90) console.log("O Grade");
+// // // // // // else if(mark>=80) console.log("A Grade");
+// // // // // // else if(mark>=70) console.log("B Grade");
+// // // // // // else console.log("Fail");
 
-
-// // // // // // i = 1;
-// // // // // // do{
-// // // // // //   console.log("do..wile");
-  
-// // // // // // }while(i===0);
-
-// // // // // // for(let i =1; i<=10; i++){
-// // // // // //   if(i%2==0)console.log(i);
-  
-// // // // // // }
-// // // // // // i =9;
-// // // // // // if(i%2==0) console.log("even");
-// // // // // // else console.log("odd");
-
-// // // // // let mark = 70;
-// // // // // if(mark>=90) console.log("O Grade");
-// // // // // else if(mark>=80) console.log("A Grade");
-// // // // // else if(mark>=70) console.log("B Grade");
-// // // // // else console.log("Fail");
-
-// // // // // let day = 6;
-// // // // // switch(day){
-// // // //  // case 1:{
-// // // // //     console.log("Sunday");
+// // // // // // let day = 6;
+// // // // // // switch(day){
+// // // // //  // case 1:{
+// // // // // //     console.log("Sunday");
             
-// // // // //   }
-// // // // //   case 2:{
-// // // // //     console.log("Monday");
-// // // // //   }
-// // // // //   case 3:{
-// // // // //     console.log("Tuesday");
-// // // // //   }
-// // // // //   case 4:{
-// // // // //     console.log("Wednesday");
-// // // // //   }
-// // // // //   case 5:{
-// // // // //     console.log("Thrusday");
-// // // // //   }
-// // // // //   case 6:{
-// // // // //     console.log("Friday");
-// // // // //   }
-// // // // //   case 7:{
-// // // // //     console.log("Saturday");
-// // // //   //     break;
-// // // // //   }
-// // // // //   default: {
-// // // // //     console.log("invalid input");
-// // // // //     break;
+// // // // // //   }
+// // // // // //   case 2:{
+// // // // // //     console.log("Monday");
+// // // // // //   }
+// // // // // //   case 3:{
+// // // // // //     console.log("Tuesday");
+// // // // // //   }
+// // // // // //   case 4:{
+// // // // // //     console.log("Wednesday");
+// // // // // //   }
+// // // // // //   case 5:{
+// // // // // //     console.log("Thrusday");
+// // // // // //   }
+// // // // // //   case 6:{
+// // // // // //     console.log("Friday");
+// // // // // //   }
+// // // // // //   case 7:{
+// // // // // //     console.log("Saturday");
+// // // // //   //     break;
+// // // // // //   }
+// // // // // //   default: {
+// // // // // //     console.log("invalid input");
+// // // // // //     break;
     
-// // // // //   }
+// // // // // //   }
 
-// // // // // }
+// // // // // // }
 
-// // // // //i = 8;
-// // // // //let result =(i%2==0)?"Even":"Odd";
+// // // // // //i = 8;
+// // // // // //let result =(i%2==0)?"Even":"Odd";
 
 
-// // // // i=8;
-// // // // let result =(i%2===0)?"Even":"Odd";
-// // // // console.log(result)
+// // // // // i=8;
+// // // // // let result =(i%2===0)?"Even":"Odd";
+// // // // // console.log(result)
 
-// // // mark = 80;
-// // // let result1 = (mark>=90)?"O Grade":(mark>=80)?"A Grade":(mark>=70)?"B Grade":(mark>=35)?"Pass":"Fail";
-// // // console.log(result1);
+// // // // mark = 80;
+// // // // let result1 = (mark>=90)?"O Grade":(mark>=80)?"A Grade":(mark>=70)?"B Grade":(mark>=35)?"Pass":"Fail";
+// // // // console.log(result1);
 
-// // function add(){
-// //   console.log(10+20);
+// // // function add(){
+// // //   console.log(10+20);
   
-// // }
-// // add();
-// // let add =(a,b)=>{
-// //   console.log(a+b);
+// // // }
+// // // add();
+// // // let add =(a,b)=>{
+// // //   console.log(a+b);
   
-// // }
-// // add()
-// var arr1 = [10,20,30];
-// var arr2 = [...arr1,40,50,60];
-// console.log(arr2);
-// var[m1,m2,m3,m4,m5] = [89,90,99,96,87]
-// console.log(m1);
-// console.log(m2);
-// console.log(m3);
-// console.log(m4);
-// console.log(m5);
+// // // }
+// // // add()
+// // var arr1 = [10,20,30];
+// // var arr2 = [...arr1,40,50,60];
+// // console.log(arr2);
+// // var[m1,m2,m3,m4,m5] = [89,90,99,96,87]
+// // console.log(m1);
+// // console.log(m2);
+// // console.log(m3);
+// // console.log(m4);
+// // console.log(m5);
 
 
-var {name,age,Dept}={
-   name : "nagi",
-   age : 19,
-   Dept:["cs","ds"]
+// var {name,age,Dept}={
+//    name : "nagi",
+//    age : 19,
+//    Dept:["cs","ds"]
 
-}
-console.log(name);
-console.log(age);
-console.log(Dept);
+// }
+// console.log(name);
+// console.log(age);
+// console.log(Dept);
 
 
 
